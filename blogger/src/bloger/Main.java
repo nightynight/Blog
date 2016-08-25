88ca97bc2227e9afd14b9166a9fb90ca47ae8951
nightynight <nightynight_cc@163.com>
2016-08-25 23:04:15 +0800

    update
    update

---
+++ blogger/src/bloger/Main.java
@@ -0,0 +1,10 @@
+package bloger;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		FileParse.MakeHtml("../");
+	}
+}
